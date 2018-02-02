c9caa9b830f4abc39015e97a8dc1b3746531de60
WitmerDesigns <patrick@witmerdesigns.com>
2018-02-02 09:39:55 -0700

    Morning Coffee fun

---
+++ index.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch');
+
+(() => {
+  console.log('start!');
+  fetch('https://www.reddit.com/r/skullcandy.json').then((response) => {
+    console.log('response: ', response);
+    return response.json();
+  }).then((json) => {
+    console.log('JSON', json);
+    console.log('posts: ', json.data);
+    const posts = json.data.children;
+
+    for (var x in posts) {
+      console.log('POST #', x);
+      console.log('title: ', posts[x].data.title);
+      console.log('url: ', posts[x].data.url);
+    }
+  })
+})();
